fb82ed77484c33973666db53bed32fc7e2f4ef52
kojikojiXX <kouji.smile@icloud.com>
2023-02-15 21:30:07 +0900

    add file

---
+++ studio04/script.js
@@ -0,0 +1,13 @@
+/*jslint browser */
+
+// CS 3312, spring 2023
+// Studio 4
+// YOUR NAME: Koji Nohara
+
+// All the code below will be run once the page content finishes loading.
+document.addEventListener("DOMContentLoaded", function () {
+  "use strict";
+
+  // Add your JavaScript code here.
+
+});
